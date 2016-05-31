b8e9e0699949203ccfd6366d0d8265259a3a7126
spandanb <spandan@savinetwork.ca>
2016-05-31 16:35:49 -0400

    bug fix

--- create_interm_files.py
+++ create_interm_files.py
@@ -9,42 +9,42 @@ def create_hosts_files():
     nodes_file      = "nodes.yaml"
 
     print "Creating host files..."
-    
+
     nodes = read_yaml(filepath=nodes_file)
 
     master_ip    = next((node['ip'] for node in nodes if node['role'] == 'master'), None)
     webserver_ip = next((node['ip'] for node in nodes if node['role'] == 'webserver'), None)
 
-    #master hosts file 
+    #master hosts file
     with open(master_hosts, 'w') as fileptr:
         fileptr.write("[master]\n")
         fileptr.write("{} ansible_user=ubuntu \n\n".format(master_ip))
 
-    #master run file 
+    #master run file
     with open(master_run , 'w') as fileptr:
         fileptr.write("#!/bin/bash\n")
         fileptr.write(
             'ansible-playbook -i hosts --extra-vars "master_ip={}" master.yaml\n'
             .format(master_ip))
-    os.chmod(wordpress_run, 0774)
+    os.chmod(master_run, 0774)
 
-    #wordpress hosts file 
+    #wordpress hosts file
     with open(wordpress_hosts, 'w') as fileptr:
         for role in ["gateway", "firewall", "webserver"]:
             fileptr.write("[{}]\n".format(role))
             matches = filter(lambda node: node["role"] == role, nodes)
-            
+
             for node in matches:
                 fileptr.write("{} ansible_user=ubuntu \n\n".format(node['ip']))
-            
-    #wordpress run file    
+
+    #wordpress run file
     with open(wordpress_run, 'w') as fileptr:
         fileptr.write("#!/bin/bash\n")
         fileptr.write(
             'ansible-playbook -i hosts --extra-vars "webserver_ip={}" wordpress3.yaml\n'
             .format(webserver_ip))
     os.chmod(wordpress_run, 0774)
-    
+
 
 if __name__ == "__main__":
     create_hosts_files()
